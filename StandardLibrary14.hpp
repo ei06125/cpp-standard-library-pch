e37dd434512a22c50dc7d4ebbd20e91d925e52b0
Pedro André Oliveira <ei06125@gcloud.fe.up.pt>
2022-04-09 11:44:21 +0100

    Merged all standards into single pch

--- StandardLibrary14.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-/// ===========================================================================
-/// @section Thread support library
-/// ===========================================================================
-#include <shared_mutex> // Shared mutual exclusion primitives
